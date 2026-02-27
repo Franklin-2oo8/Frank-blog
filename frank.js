5613648093586d5c49391ed0d67b03f0b28998af
Franklin-2oo8 <frankdageek@yahoo.com>
2026-02-27 20:03:31 +0100

    Add files via upload

---
+++ frank.js
@@ -0,0 +1,8 @@
+function openMenu(){
+  let open = document.getElementById("menu");
+  open.style.display = "block"
+}
+function closeMenu(){
+  let close = document.getElementById("menu");
+  close.style.display = "none"
+}
